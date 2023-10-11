c41a9b690e76bd18ba32287f53028f0fc8321653
Your Name <jxtsly@gmail.com>
2023-10-11 20:04:18 +0200

    simple button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Ecomm Project</h1>
+    return ( 
+    <div className = "App" >
+        <h1> Ecomm Project </h1> 
+        <button> simple button </button>
+        <Button>Bootstrap</Button> 
     </div>
-  );
+    );
 }
 
 export default App;
