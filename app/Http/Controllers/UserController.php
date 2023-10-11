9612e2126dce4cf22289dd1458e87821bc96fc50
Your Name <jxtsly@gmail.com>
2023-10-11 22:52:54 +0200

    Initial commit for the backend code

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    function register(Request $request)  {
+        $highestId = DB::table('users')->max('id'); // Find the highest existing "id".
+        $newId = $highestId + 1; // Generate a new, unique "id."
+
+        $user = new User;
+        $user->id = $newId; 
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+        return $user;
+    }
+}
