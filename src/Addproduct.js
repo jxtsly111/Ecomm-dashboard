ebdcdb814cd2aa32fe5c91be89e9f72bff2bc7a2
Your Name <jxtsly@gmail.com>
2023-10-13 11:08:55 +0200

    form integration

--- src/Addproduct.js
+++ src/Addproduct.js
@@ -7,6 +7,48 @@ function AddProduct() {
             <h1>
                 AddProduct 
             </h1>
+            <div style={containerStyle}>
+                <input
+                    type="text"
+                    className="form-control"
+                    // value={name}
+                    // onChange={(e) => setName(e.target.value)}
+                    style={inputStyle}
+                    placeholder="name"
+                />
+                <br />
+                <input
+                    type="file"
+                    className="form-control"
+                    // value={password}
+                    // onChange={(e) => setPassword(e.target.value)}
+                    style={inputStyle}
+                    placeholder="file"
+                />
+                <br />
+                <input
+                    type="text"
+                    className="form-control"
+                    // value={email}
+                    // onChange={(e) => setEmail(e.target.value)}
+                    style={inputStyle}
+                    placeholder="price"
+                />
+                <br />
+
+                <input
+                    type="text"
+                    className="form-control"
+                    // value={email}
+                    // onChange={(e) => setEmail(e.target.value)}
+                    style={inputStyle}
+                    placeholder="description"
+                />
+                <br />
+                <button className="btn btn-primary">
+                    Add Product
+                </button>
+            </div>
         </div>
         </>
         
