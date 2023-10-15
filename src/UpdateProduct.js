3062e9ffae86bdd4769e7fd2fccf11da20e0f380
Your Name <jxtsly@gmail.com>
2023-10-15 11:22:37 +0200

    update product api

--- src/UpdateProduct.js
+++ src/UpdateProduct.js
@@ -1,17 +1,38 @@
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
-import { withRouter } from "react-router-dom";
- function UpdateProduct() {
-    return(
+import { useParams } from "react-router-dom";
+
+function UpdateProduct() {
+    const { id } = useParams();
+    const [data, setData] = useState({ name: "" }); // Initialize data state
+
+    console.warn("props", id);
+
+    useEffect(() => {
+        // Use a separate function to fetch the data
+        async function fetchData() {
+            try {
+                const result = await fetch(`http://127.0.0.1:8000/api/product/${id}`);
+                const data = await result.json();
+                setData(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+
+        fetchData(); // Call the fetchData function
+    }, [id]); // Add 'id' to the dependency array to rerun the effect when 'id' changes
+
+    return (
         <>
-         <Header />
-        <div>
-            <h1>
-                UpdateProduct
-            </h1>
-        </div>
+            <Header />
+            <div>
+                <h1>UpdateProduct</h1>
+                <input type="text" defaultValue={data.name} /> <br /> <br />
+                <button>Update Product</button>
+            </div>
         </>
-       
-    )
+    );
 }
 
-export default UpdateProduct
+export default UpdateProduct;
