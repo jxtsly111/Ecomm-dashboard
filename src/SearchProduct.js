602ab040068d0b6adb236e3f079c0e7c1844d4d8
Your Name <jxtsly@gmail.com>
2023-10-16 10:13:15 +0200

    React project tutorial # Search Auto Complete

--- src/SearchProduct.js
+++ src/SearchProduct.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import { useState } from "react";
+import { Table } from "react-bootstrap";
 function SearchProduct() {
 
     const [data, setData]=useState([])
@@ -31,7 +32,32 @@ function SearchProduct() {
         </h1>
         <br />
         <div style={containerStyle}>
-        <input type="text" onChange={(e)=>search(e.target.value)} className="form-control" style={inputStyle} placeholder="Search Product" />
+        <input type="text" onChange={(e)=>search(e.target.value)} className="form-control" style={inputStyle} placeholder="Search Product" /><br />
+
+        <Table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        data.map((item) =>
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.price}</td>
+                                <td>{item.description}</td>
+                                <td><img style={{ width: 80 }} src={"http://127.0.0.1:8000/" + item.file_path}></img></td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </Table>
         </div>
        
         </div>
